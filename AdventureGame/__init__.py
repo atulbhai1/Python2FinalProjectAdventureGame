27d89b530a0c11e48d02f8f2d32535c57e09676a
atulbhai1 <atulbhai.nitin@gmail.com>
2025-04-22 22:42:47 -0400

    Made stuff

---
+++ AdventureGame/__init__.py
@@ -0,0 +1,23 @@
+#----------
+#AdventureGame/__init__.py
+#----------
+#Description:
+"""This file is the central file of this adventure game and will merely serve as a central hub to be a high level
+gateway to other files in this package."""
+#----------
+#Created:
+# 4/22/2025
+#----------
+#Last Modified:
+# 4/22/2025
+#----------
+#Interpreter:
+#Python 3.11
+#----------
+#Imports
+
+#----------
+
+def main():
+    """This function pushes the user to the start page and deals with any exceptions that aren't covered
+    by the rest of the program."""
