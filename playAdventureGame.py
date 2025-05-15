88c1ac816a4609911e18c445e5bef8fdd58d512b
atulbhai1 <atulbhai.nitin@gmail.com>
2025-05-14 20:17:28 -0400

    FINISHED!!!! FINALLY!!!!

--- playAdventureGame.py
+++ playAdventureGame.py
@@ -8,10 +8,10 @@
 # 5/3/2025
 #----------
 #Last Modified:
-# 5/3/2025
+# 5/14/2025
 #----------
 #Version #:
-#0.0
+#0.1
 #----------
 #Interpreter:
 #Python 3.11
@@ -22,6 +22,27 @@ import AdventureGame
 
 main = AdventureGame.main#Reassign a function to have another name!
 
-#For testing purposes, if this file is being run on its own, automatically run main()
+password = "AtulIsCool1234"
+
+#Standard practice, if this file is being run on its own(as it needs to be), get the password sign in and run main()
 if __name__ == "__main__":
-    main()
+    user_password_entry = input("Welcome to Atul's Adventure Game! Enter your password to start:")#Get the user to enter a password to begin
+
+    if password == user_password_entry:  # If they entered the right password
+        repeat = True#By default, keep playing this game forever!!!
+        while repeat:#While it should repeat
+            print()
+            main()
+            user_choice = input("Thanks for playing. Want to play AGAIN???(Enter 'Y' to play again, or enter anything else to leave)")
+            if user_choice.lower().strip() != 'y':#If they didn't choose to continue
+                repeat = False#Stop looping
+                print("Goodbye, please return to this wonderful game sooner rather than later.")
+                #--Fin.--
+            #If they did choose to continue, it will automatically as repeat is True by default
+    else:#Password entered is wrong
+        print("You shouldn't be here. You've been very very bad.")
+
+        #--Fin.--
+
+
+
